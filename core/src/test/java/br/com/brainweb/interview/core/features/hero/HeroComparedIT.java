1c0ee484b3f3eb202bc4d7cd9f99f004baacf440
fandriosi <fabio.andriosi@gmail.com>
2020-10-05 07:37:36 -0300

    Finalizado HeroCompared test

--- core/src/test/java/br/com/brainweb/interview/core/features/hero/HeroComparedIT.java
+++ core/src/test/java/br/com/brainweb/interview/core/features/hero/HeroComparedIT.java
@@ -1,12 +1,65 @@
 package br.com.brainweb.interview.core.features.hero;
 
 import br.com.brainweb.interview.core.features.powerstats.PowerStatsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.brainweb.interview.core.util.HeroComparad;
+import br.com.brainweb.interview.model.Hero;
+import br.com.brainweb.interview.model.PowerStats;
+import org.junit.Test;
+import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.Assert.*;
+import java.util.Date;
+
+@ActiveProfiles("it")
 public class HeroComparedIT {
-    @Autowired
-    private PowerStatsRepository powerStatsRepository;
-    @Autowired
-    private HeroRepository heroRepository;
+    private Hero capitao;
+    private Hero thor;
+    private PowerStats capitaoPower;
+    private PowerStats thorPower;
 
+    public HeroComparedIT() {
+        this.capitao = new Hero();
+        this.thor = new Hero();
+        this.capitaoPower = new PowerStats();
+        this.thorPower = new PowerStats();
+    }
+
+    private Hero createCapitao(){
+        capitaoPower.setAgility(5);
+        capitaoPower.setStrength(8);
+        capitaoPower.setDexterity(8);
+        capitaoPower.setIntelligence(9);
+        capitaoPower.setCreated_at(new Date(2020,9,05));
+        capitaoPower.setUpdated_at(new Date(2020,9,05));
+        capitao.setName("Capitão America");
+        capitao.setRace(Race.HUMAN.name());
+        capitao.setPowerStats(capitaoPower);
+        capitao.setEnabled(true);
+        capitao.setCreated_at(new Date(2020,9,05));
+        capitao.setCreated_at(new Date(2020,9,05));
+        return capitao;
+    }
+    private Hero createThor(){
+        thorPower.setAgility(4);
+        thorPower.setStrength(10);
+        thorPower.setDexterity(8);
+        thorPower.setIntelligence(8);
+        thorPower.setCreated_at(new Date(2020,9,05));
+        thorPower.setUpdated_at(new Date(2020,9,05));
+        thor.setName("Thor");
+        thor.setRace(Race.DIVINE.name());
+        thor.setPowerStats(thorPower);
+        thor.setEnabled(true);
+        thor.setCreated_at(new Date(2020,9,05));
+        thor.setCreated_at(new Date(2020,9,05));
+        return thor;
+    }
+    @Test
+    public void comparedHeroes(){
+        Hero hero = this.createCapitao();
+        Hero hero1 = createThor();
+        Hero hero2 = new HeroComparad().getHeroCompared(hero, hero1);
+        assertEquals("Thor", hero2.getName());
+        assertEquals(Race.DIVINE.name(), hero2.getRace());
+    }
 }
